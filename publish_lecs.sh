845ce893ce55e81cc5e63760a569f48686f37654
Brett Longworth <blongworth@whoi.edu>
2025-06-30 16:24:02 -0400

    first commit

---
+++ publish_lecs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# render and publish LECS data from website
+# use data from the last week
+# publish to https://blongworth.github.io/machinelab-work/
+
+QUARTO_PATH=/Applications/quarto/bin/quarto
+
+$QUARTO_PATH render gems_parse_web.qmd -P start_date:$(date -v-7d "+%Y-%m-%d")
+#$QUARTO_PATH render read_parse_web.qmd
+$QUARTO_PATH publish gh-pages gems_parse_web.qmd --no-render --no-prompt --no-browser
