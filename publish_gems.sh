8abab069691b2d3926f694a3a880cbe4e7a8c2ad
Brett Longworth <blongworth@whoi.edu>
2025-07-13 10:08:30 -0400

    use script for a week of data

--- publish_gems.sh
+++ publish_gems.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# render and publish LECS data from website
-# use data from the last week
-# publish to https://blongworth.github.io/machinelab-work/
-
-QUARTO_PATH=/Applications/quarto/bin/quarto
-
-$QUARTO_PATH render index.qmd -P start_date:$(date -v-7d "+%Y-%m-%d")
-#$QUARTO_PATH render index.qmd
-$QUARTO_PATH publish gh-pages --no-render --no-prompt --no-browser
+quarto render index.qmd -P start_date:$(date -v-7d "+%Y-%m-%d")
+quarto publish gh-pages --no-render --no-prompt --no-browser
